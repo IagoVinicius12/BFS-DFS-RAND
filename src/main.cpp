fa755ab719abd07b2bb86ced2dbc6eae5175e3fd
Rafael Moreira <127882640+Guiliard@users.noreply.github.com>
2023-05-22 20:35:11 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <chrono>
 #include "Randomico.hpp"
 #include "Profundidade.hpp"
 #include "Largura.hpp"
 
 int main()
 {
+    chrono :: steady_clock :: time_point inicio_total = chrono::steady_clock :: now();
     Randomico *randomico = new Randomico();
     Profundidade *profundo = new Profundidade();
     Largura *largura = new Largura();
@@ -22,10 +24,12 @@ int main()
         mat[i] = new Regiao[tamanho];
 
     cout << "\nLARGURA" << endl;
+    chrono :: steady_clock :: time_point inicio_largura = chrono::steady_clock :: now();
     largura->Leitura(arq,mat);
     largura->andando(mat);
     largura->criatrajetoria(mat);
     largura->vermatriz(mat);
+    chrono :: steady_clock :: time_point fim_largura = chrono :: steady_clock :: now();
     arq.close();
 
     arq.open(nomearq);
@@ -35,10 +39,12 @@ int main()
     profundo->settamanho(tamanho);
 
     cout << "\nPROFUNDIDADE" << endl;
+    chrono :: steady_clock :: time_point inicio_profundidade = chrono::steady_clock :: now();
     profundo->lendomatriz(arq, mat);
     profundo->andando(mat);
     profundo->criatrajetoria(mat);
     profundo->vermatriz(mat);
+    chrono :: steady_clock :: time_point fim_profundidade = chrono :: steady_clock :: now();
     arq.close();
 
     arq.open(nomearq);
@@ -48,9 +54,24 @@ int main()
     randomico->setTamanho(tamanho);
 
     cout << "\nRANDÔMICO" << endl;
+    chrono :: steady_clock :: time_point inicio_randomico = chrono::steady_clock :: now();
     randomico->Leitura(arq, mat);
     randomico->Movimento(mat);
     randomico->CriaTrajetoria(mat);
     randomico->VerMatriz(mat);
+    chrono :: steady_clock :: time_point fim_randomico = chrono :: steady_clock :: now();
     arq.close();
+
+    chrono :: steady_clock :: time_point fim_total = chrono :: steady_clock :: now();
+
+    chrono :: duration <double, milli> tempo_largura = fim_largura - inicio_largura;
+    chrono :: duration <double, milli> tempo_profundidade = fim_profundidade - inicio_profundidade;
+    chrono :: duration <double, milli> tempo_randomico = fim_randomico - inicio_randomico;
+    chrono :: duration <double, milli> tempo_total = fim_total - inicio_total;
+
+    cout << "\nTEMPO DE EXECUÇÃO" << endl;
+    cout << "BFS: " << fixed << tempo_largura.count() << " ms"<< endl;
+    cout << "DFS: " << fixed << tempo_profundidade.count() << " ms"<< endl;
+    cout << "RAND: " << fixed << tempo_randomico.count() << " ms"<< endl;
+    cout << "TOTAL: " << fixed << tempo_total.count() << " ms"<< endl;
 }
